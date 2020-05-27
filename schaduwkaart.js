18869b52dd9917ab35418cf5f9bc04508ae4e118
Michiel Pauwels <USER3102@aquafinad.aquafin.be>
2020-05-27 15:23:58 +0200

    Issue #3

--- schaduwkaart.js
+++ schaduwkaart.js
@@ -7,10 +7,10 @@ var drawPlot;
 var drawObstacles;
 var geoJson = new ol.format.GeoJSON();
 
-const nb = 100;
+const nb_angles = 100;
 
 function shadeStyle(feature) {
-    const shade = 255 - (feature.getProperties().shade / nb) * 255;
+    const shade = 255 - (feature.getProperties().shade / nb_angles) * 255;
     return [new ol.style.Style({
         fill: new ol.style.Fill({
             color: `rgba(${shade}, ${shade}, ${shade}, 0.8)`
@@ -103,66 +103,83 @@ function activateDrawObstacles() {
 
 function calculateShadow() {
 
-    let plotGeom = plotLayer.getSource().getFeatures()[0].getGeometry();
+    let plotGeom = new ol.geom.MultiPolygon(plotLayer.getSource().getFeatures().map(feat => feat.getGeometry()));
     const plotExtent = plotGeom.getExtent();
     const center = [(plotExtent[0] + plotExtent[2])/2, (plotExtent[1] + plotExtent[3])/2];
 
-    const gridSize = 100;
-    const delta = Math.abs(plotExtent[0] - plotExtent[2]) / gridSize;
-    const grid = [];
-    const startX = Math.min(plotExtent[0], plotExtent[2]);
-    const startY = Math.max(plotExtent[1], plotExtent[3])
-    let currentY = startY;
-    let yIdx = 0;
-    while(currentY > Math.min(plotExtent[1], plotExtent[3])) {
-        grid.push(...new Array(gridSize).fill(0).map((el, xIdx) => (geoJson.readFeature({
-            type: "Feature",
-            geometry: {
-                type: "Polygon",
-                coordinates: [[
-                    [startX + xIdx * delta, startY - yIdx * delta],
-                    [startX + (xIdx + 1) * delta, startY - yIdx * delta],
-                    [startX + (xIdx + 1) * delta, startY - (yIdx + 1) * delta],
-                    [startX + xIdx * delta, startY - (yIdx + 1) * delta],
-                    [startX + xIdx * delta, startY - yIdx * delta]
-                ]]
-            },
-            properties: {
-                shade: 0
-            }
-        }))).filter((el, xIdx) => plotGeom.intersectsCoordinate(
-            [startX + (xIdx + 0.5) * delta, startY - (yIdx + 0.5) * delta]
-        )));
-        yIdx++;
-        currentY = currentY - delta;
-    }
-    gridLayer.getSource().addFeatures(grid);
+    const grid = getGrid();
 
     const obstacleGeom = obstacleLayer.getSource().getFeatures()
-        .map(feat => ({
-            floor: getRelativeCoordinates(feat.getGeometry().getCoordinates()[0]),
-            roof: feat.getGeometry().getCoordinates()[0]
-                .map(coord => [coord[0] - center[0], coord[1] - center[1], Number(feat.getProperties().height)]),
-        })
+        .map(feat => {
+            const relativeGeomCoord = getRelativeCoordinates(feat.getGeometry()).getCoordinates()[0];
+            return {
+                floor: relativeGeomCoord,
+                roof: relativeGeomCoord.map(coord => [...coord, Number(feat.getProperties().height)]),
+            };
+        }
     );
-    plotGeom = [getRelativeCoordinates(plotGeom.getCoordinates()[0])];
-    // console.log(obstacleGeom);
 
-    new Array(nb + 1).fill(0).map((el, idx) => (idx/nb) * Math.PI).forEach(angle => {
-        const solar = getSolarCoordinatesForAngle(angle);
-        const shadows = obstacleGeom
-            .map(obstacle => turf.intersect(
-                turf.polygon(plotGeom),
-                getShadowForObstacle(solar, obstacle.roof, obstacle.floor)
-            )).filter(shadow => !!shadow);
-        const shadowGeom = shadows.map(shadow => new ol.geom.Polygon(getAbsoluteCoordinates(shadow)));
+    new Array(nb_angles + 1).fill(0).map((el, idx) => (idx/nb_angles) * Math.PI)
+    .map(angle => getSolarCoordinatesForAngle(angle))
+    .forEach(solar => checkGridForShadows(grid, getShadowsForObstacles(solar, obstacleGeom)))
+
+    function getShadowsForObstacles(solar, obstacleGeom) {
+        const shadows = [];
+        obstacleGeom.forEach(obstacle => {
+            const castShadow = getShadowForObstacle(solar, obstacle.roof, obstacle.floor);
+            plotGeom.getPolygons().forEach(plot => {
+                const overlappingShadow = turf.intersect(turf.polygon(plot.getCoordinates()), castShadow);
+                if (!!overlappingShadow) {
+                    shadows.push(geoJson.readFeature(overlappingShadow).getGeometry());
+                }
+            })
+        });
+        return shadows;
+    }
+
+    function checkGridForShadows(grid, shadows) {
         grid.forEach(gridEl => {
             const shade = gridEl.getProperties().shade;
-            if (shadowGeom.some(sgeom => sgeom.intersectsCoordinate(gridEl.getGeometry().getFirstCoordinate()))) {
+            if (shadows.some(sgeom => sgeom.intersectsCoordinate(ol.extent.getCenter(gridEl.getGeometry().getExtent())))) {
                 gridEl.setProperties({shade: shade + 1});
             }
-        })
-    })
+        });
+    }
+
+    function getGrid() {
+        const gridSize = 100;
+        const delta = Math.abs(plotExtent[0] - plotExtent[2]) / gridSize;
+        const grid = [];
+        const startX = Math.min(plotExtent[0], plotExtent[2]);
+        const startY = Math.max(plotExtent[1], plotExtent[3])
+        let currentY = startY;
+        let yIdx = 0;
+        while(currentY > Math.min(plotExtent[1], plotExtent[3])) {
+            grid.push(...new Array(gridSize).fill(0).map((el, xIdx) => (geoJson.readFeature({
+                type: "Feature",
+                geometry: {
+                    type: "Polygon",
+                    coordinates: [[
+                        [startX + xIdx * delta, startY - yIdx * delta],
+                        [startX + (xIdx + 1) * delta, startY - yIdx * delta],
+                        [startX + (xIdx + 1) * delta, startY - (yIdx + 1) * delta],
+                        [startX + xIdx * delta, startY - (yIdx + 1) * delta],
+                        [startX + xIdx * delta, startY - yIdx * delta]
+                    ]]
+                },
+                properties: {
+                    shade: 0
+                }
+            }))).filter((el, xIdx) => plotGeom.intersectsCoordinate(
+                [startX + (xIdx + 0.5) * delta, startY - (yIdx + 0.5) * delta]
+            )));
+            yIdx++;
+            currentY = currentY - delta;
+        }
+        gridLayer.getSource().clear();
+        gridLayer.getSource().addFeatures(grid);
+        return grid;
+    }
 
     function getSolarCoordinatesForAngle(phi) {
         const saa = 0.68 //Solar Altitude Angle (rad)
@@ -178,8 +195,19 @@ function calculateShadow() {
 
     function getShadowForObstacle(solarPoint, obs_roof, obs_ground) {
         const shadowPoints = obs_roof.map(roofPoint => getShadowPoint(solarPoint, roofPoint));
-        return convexHull(obs_ground.concat(shadowPoints));
+        return geoJson.writeFeatureObject(
+            new ol.Feature(getAbsoluteCoordinates(
+                geoJson.readFeature(convexHull(obs_ground.concat(shadowPoints))).getGeometry()
+            ))
+        );
     }
+
+    function getSolarLine(solarPoint) {
+        return new ol.Feature(getAbsoluteCoordinates(
+            new ol.geom.LineString([[solarPoint[0], solarPoint[1]], [0, 0]])
+        ));
+    }
+
     function getShadowPoint(solarPoint, roofPoint) {
         const xz = solarPoint[0];
         const yz = solarPoint[1];
@@ -196,16 +224,12 @@ function calculateShadow() {
     }
 
     function getRelativeCoordinates(polygon) {
-        return polygon.map(coord => [coord[0] - Number(center[0]), coord[1] - Number(center[1])]);
+        polygon.translate(- Number(center[0]), - Number(center[1]));
+        return polygon;
     }
 
-    function getAbsoluteCoordinates(feature) {
-        if (feature.geometry.type === 'Polygon') {
-            return feature.geometry.coordinates.map(ring => ring.map(coord => [coord[0] + Number(center[0]), coord[1] + Number(center[1])]));
-        } else {
-            return feature.geometry.coordinates.map(polygon => 
-                polygon.map(ring => ring.map(coord => [coord[0] + Number(center[0]), coord[1] + Number(center[1])])));
-        }
-        return ;
+    function getAbsoluteCoordinates(polygon) {
+        polygon.translate(Number(center[0]),Number(center[1]));
+        return polygon;
     }
 }
